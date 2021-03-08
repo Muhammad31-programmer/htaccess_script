eda35cc5b6045476241ec21c95f2bd2199c8871a
Muhammad31-programmer <79897510+Muhammad31-programmer@users.noreply.github.com>
2021-03-08 14:26:13 +0500

    Initial commit

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<nav>
+	
+	<li><a href="index">Home</a></li>
+	<li><a href="about">About</a></li>
+	<li><a href="contact">Contact</a></li></nav>
+<br>
+
+<h2>About Us page</h2>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<nav>
+	
+	<li><a href="index">Home</a></li>
+	<li><a href="about">About</a></li>
+	<li><a href="contact">Contact</a></li>
+</nav>
+</body>
+</html>
